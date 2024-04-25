69b11f54ae4df4f73ddf31536344440250d83cd3
joyeonw <jbg0220@naver.com>
2024-04-25 12:39:34 +0900

    4.25 목요일 오전

--- src/main/java/com/green/controller/BusanBIMSController.java
+++ src/main/java/com/green/controller/BusanBIMSController.java
@@ -11,19 +11,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.green.dto.BusInfoDto;
+
 @Controller
 public class BusanBIMSController {
 	
 	@RequestMapping("/GetBus")
 	@ResponseBody
-	public String getBus( ) throws IOException {
+	public String getBus( 
+			BusInfoDto dto
+//		 	String arsno, String bstopnm, String numOfRows, String pageNo
+			) throws IOException {
 		String serviceKey = "KZ1wFMub56Z%2FluONbh%2F%2B5oKcDD0VjYDvrXcKulj3%2B8W%2FR3b9bKzvMqRcc%2BNA3iTwOW1mE6mgOyHLXMBJn0Pygw%3D%3D";
 		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/6260000/BusanBIMS/busStopList"); /*URL*/
         urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + serviceKey); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("bstopnm","UTF-8") + "=" + URLEncoder.encode("부산시청", "UTF-8")); /*정류소 명*/
-        urlBuilder.append("&" + URLEncoder.encode("arsno","UTF-8") + "=" + URLEncoder.encode("13708", "UTF-8")); /*정류소 번호*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode(dto.getPageNo(), "UTF-8")); /*페이지번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode(dto.getNumOfRows(), "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("bstopnm","UTF-8") + "=" + URLEncoder.encode(dto.getBstopnm(), "UTF-8")); /*정류소 명*/
+        urlBuilder.append("&" + URLEncoder.encode("arsno","UTF-8") + "=" + URLEncoder.encode(dto.getArsno(), "UTF-8")); /*정류소 번호*/
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
